fb5da8cc4ad94292d657580610c83c78957d47b0
Martin <martin@pythonforbiologists.com>
2017-06-29 16:37:33 +0000

    add files

---
+++ get_taxids.py
@@ -0,0 +1,165 @@
+import pickle
+import argparse
+
+
+
+
+
+def get_children(taxon):
+	# print('taxon is ~~' + taxon + '~~')
+	taxon_taxid = name2taxid.get(taxon, 'none')
+	# print(' taxon taxid is ' + str(taxon_taxid))
+	children = parent2child.get(taxon_taxid, [])
+	return [taxid2name[x] for x in children]
+
+def get_children_multiple(taxon, count):
+	current_list = [taxon]
+	while count > 0:
+		new_list = []
+		for current_taxon in current_list:
+			new_list.extend(get_children(current_taxon))
+		current_list = new_list
+		count += -1
+	return new_list
+
+
+def get_parent(taxon):
+	taxon_taxid = name2taxid.get(taxon, 'none')
+	taxon_parent_taxid = taxid2parent.get(taxon_taxid, 'none')
+	return taxid2name.get(taxon_parent_taxid, 'none')
+
+def get_parent_multiple(taxon, count):
+	current_taxon = taxon
+	while count > 0:
+		current_taxon = get_parent(current_taxon)
+		count += -1
+	return current_taxon
+
+def get_siblings(taxon):
+	return filter( lambda x : x != taxon, get_children(get_parent(taxon)))
+
+def get_siblings_multiple(taxon, count):
+	parent = get_parent_multiple(taxon, count)
+	return filter( lambda x : x != taxon, get_children_multiple(parent, count))
+
+# recursive function to get all the children of a given taxid
+def get_children_recursive(taxid):
+    result = []
+    # for all children of this taxid...
+    for child in parent2child[taxid]:
+        # first add the child itself
+        result.append(child)
+        # then if the child has children...
+        if child in parent2child:
+            # add the children of the child
+            # note that we use extend() here rather than append()
+            # extend() is the equivalent of concatenating two lists
+            # if we use append() here then we get a set of nested list which is not what we want!
+            result.extend(get_children_recursive(child))
+    return result
+
+def get_named_children(taxon, rank):
+	print('getting all ' + rank)
+	all_children = get_children_recursive(name2taxid[taxon])
+	return list([taxid2name[child] for child in all_children if taxid2rank[child] == rank])
+
+def get_all_parents(species):
+    result = []
+    current_taxid = species
+    found = False
+    while current_taxid != 1:
+        result.append(current_taxid)
+        parent_taxid = taxid2parent[current_taxid]
+        current_taxid = parent_taxid
+    result.append(1)
+    return result
+
+
+def find_lca(species1, species2):
+    species1_parents = get_all_parents(species1)
+    species2_parents = get_all_parents(species2)
+    for parent in species1_parents:
+        if parent in species2_parents:
+            return parent
+
+
+def find_lca_multiple(list_of_species):
+    # the lca of a single species is itself
+    # so we will set the lca to be the first species
+    current_lca = name2taxid[list_of_species[0]]
+    # now go through the list of species
+    for species in list_of_species:
+        #set the new lca to the the lca of the current species and the old lca
+        current_lca = find_lca(name2taxid[species], current_lca)
+    return taxid2name[current_lca]
+
+
+parser = argparse.ArgumentParser(
+    description='Get lists of taxids from the NCBI taxonomy'
+    )
+
+parser.add_argument('--output',  help='output file to write taxids to')
+
+parser.add_argument(
+    '--include',
+    nargs='+',
+    help="space-separated list of taxids who's descendents we want to include",
+    type=int
+    )
+
+parser.add_argument(
+    '--exclude',
+        nargs='+',
+        type=int,
+    help="space-separated list of taxids who's descendents we want to exclude"
+    )
+
+parser.add_argument(
+    '--lookup',
+    help="a taxon name who's taxid you want to look up. If this option is used the program will just output the taxid(s)"
+    )
+
+args = parser.parse_args()
+
+
+if args.lookup is not None:
+    print('reading names....', end='\r')
+    name2taxid = pickle.load( open( "name2taxid.p", "rb" ) )
+    print('loading taxonomy....done!')
+    taxids = name2taxid.get(args.lookup)
+    if taxids is None:
+        print("Couldn't find a taxid for {}".format(args.lookup))
+    elif len(taxids) > 1:
+        print("Found multiple taxids for that name:")
+        print('\n'.join(taxids))
+    else:
+        print(taxids[0])
+
+else:
+    selected_taxids = set()
+    print('reading nodes....', end='\r')
+    parent2child = pickle.load( open( "parent2child.p", "rb" ) )
+    print('reading nodes....done')
+
+    for include_taxid in args.include:
+        selected_taxids = selected_taxids.union(get_children_recursive(include_taxid))
+        print('after adding children of {} we have {} taxids'.format(
+            include_taxid, len(selected_taxids)
+        ))
+
+    for exclude_taxid in args.exclude:
+        selected_taxids = selected_taxids.difference(get_children_recursive(exclude_taxid))
+        print('after removing children of {} we have {} taxids'.format(
+            exclude_taxid, len(selected_taxids)
+        ))
+    with open(args.output, 'wt') as output_file:
+        for taxid in selected_taxids:
+            output_file.write('{}\n'.format(taxid))
+    print('wrote {} taxids to {}'.format(
+        len(selected_taxids), args.output
+    ))
+
+# taxid2name = pickle.load( open( "taxid2name.p", "rb" ) )
+# taxid2rank = pickle.load( open( "taxid2rank.p", "rb" ) )
+# taxid2parent = pickle.load( open( "taxid2parent.p", "rb" ) )
+# parent2child = pickle.load( open( "parent2child.p", "rb" ) )
---
+++ localgb.py
@@ -0,0 +1,188 @@
+from ftplib import FTP
+from io import StringIO
+from tqdm import tqdm
+import os
+import logging
+import tarfile
+import pickle
+import collections
+
+def need_to_update_release():
+    """
+    Check whether the user currently has the latest GB release (in which case
+    we only need to download the daily updates) or whether they have an old
+    release or no release, in which case we need to download everything)
+    """
+    try:
+        current_release_number = int(open('GB_Release_Number').read())
+        logging.info(
+            'You currently have GB release number {}'
+            .format(current_release_number)
+            )
+
+    except IOError:
+        logging.info('No current release, downloading the files')
+        return True
+
+    latest_release_file = StringIO()
+    ftp.retrlines('RETR GB_Release_Number', latest_release_file.write)
+    latest_release_number = int(latest_release_file.getvalue())
+    logging.info('Latest release number is {}'.format(latest_release_number))
+
+    if current_release_number == latest_release_number:
+        logging.info('You have the latest release, getting daily updates')
+        return False
+    else:
+        logging.info('New release available, downloading the files')
+        return True
+
+
+def get_daily_updates():
+    ftp.cwd('daily-nc')
+
+    files_to_download = []
+    for filename, file_info in ftp.mlsd():
+        if filename.startswith('nc') and filename.endswith('.flat.gz'):
+            logging.debug('checking ' + filename)
+            if os.path.exists(filename):
+                logging.debug("already got {}".format(filename))
+            else:
+                files_to_download.append((filename, file_info))
+    total_size = sum([int(f[1]['size']) for f in files_to_download])
+    if total_size > 0:
+        logging.info(
+            'about to download {} MB, hit enter to continue or Ctrl+C to cancel...'
+            .format(round(total_size / 10 ** 6))
+            )
+        input()
+        with tqdm(
+            files_to_download,
+            unit='files',
+            desc='Downloading {} files'.format(len(files_to_download))
+        ) as file_pbar:
+            for f in file_pbar:
+                download(f)
+
+def build_ncbi_names_dicts():
+    names = open("names.dmp")
+    name2taxid = collections.defaultdict(list)
+    taxid2name = {}
+
+    for line in names:
+        taxid, name, other_name, type = line.rstrip('\t|\n').split("\t|\t")
+        name2taxid[name].append(taxid)
+        if type == 'scientific name':
+            taxid2name[int(taxid)] = name
+
+    pickle.dump( name2taxid, open( "name2taxid.p", "wb" ) )
+    pickle.dump( taxid2name, open( "taxid2name.p", "wb" ) )
+
+def build_ncbi_nodes_dicts():
+    taxid2rank = {}
+    taxid2parent = {}
+
+    #store parent to child relationships
+    parent2child = {}
+
+    processed = 0
+    nodes = open("nodes.dmp")
+    for line in nodes:
+        taxid, parent, rank = line.rstrip('\t|\n').split("\t|\t")[0:3]
+        taxid = int(taxid)
+        parent = int(parent)
+        taxid2rank[int(taxid)] = rank
+        taxid2parent[int(taxid)] = int(parent)
+        if parent not in parent2child:
+            parent2child[parent] = []
+        parent2child[parent].append(taxid)
+
+    pickle.dump( taxid2rank, open( "taxid2rank.p", "wb" ) )
+    pickle.dump( taxid2parent, open( "taxid2parent.p", "wb" ) )
+    pickle.dump( parent2child, open( "parent2child.p", "wb" ) )
+
+
+def get_taxdump():
+    logging.debug('starting download taxdump')
+    logging.info('downloading taxonomy dump...')
+    ftp.cwd('/pub/taxonomy')
+
+    for filename, file_info in ftp.mlsd():
+        if filename == 'taxdump.tar.gz':
+            download((filename, file_info))
+
+    with tqdm(total=3, desc='extracting files from taxdump') as pbar:
+        logging.debug('extracting files from taxdump')
+        tar = tarfile.open('taxdump.tar.gz', 'r:gz')
+        tar.extractall()
+        tar.close()
+        pbar.set_description('reading names from taxonomy')
+        pbar.update(1)
+        logging.debug('finishing download taxdump')
+        logging.debug('building NCBI dicts')
+
+
+        build_ncbi_names_dicts()
+        pbar.set_description('reading nodes from taxonomy')
+        pbar.update(1)
+
+
+        build_ncbi_nodes_dicts()
+
+        pbar.update(1)
+
+
+
+
+
+def download(myfile):
+    filename, file_info = myfile
+    with tqdm(
+                total=int(file_info['size']),
+                desc=filename,
+                unit='bytes',
+                unit_scale=True,
+                leave=False
+              ) as pbar:
+        with open(filename, 'wb') as output:
+            def handle_block(block):
+                pbar.update(len(block))
+                output.write(block)
+            ftp.retrbinary('RETR ' + filename, handle_block)
+
+
+def do_update_release():
+    logging.debug('looking for divisions: {}'.format(args.divisions))
+    files_to_download = []
+    for filename, file_info in ftp.mlsd():
+        file_division = filename[2:5]
+        if (
+            filename.startswith('gb')
+            and file_division.upper() in args.divisions
+        ):
+            files_to_download.append((filename, file_info))
+
+    total_size = sum([int(f[1]['size']) for f in files_to_download])
+    logging.info(
+        'about to download {} MB, hit enter to continue or Ctrl+C to cancel...'
+        .format(round(total_size / 10 ** 6))
+        )
+    input()
+    with tqdm(
+        files_to_download,
+        unit='files',
+        desc='Downloading {} files'.format(len(files_to_download))
+    ) as file_pbar:
+        for f in file_pbar:
+            download(f)
+
+    with open('GB_Release_Number', 'wb') as output:
+        ftp.retrbinary('RETR GB_Release_Number', output.write)
+
+
+def delete_old_files():
+    logging.info('Warning, about to delete files from your old release')
+    logging.info('Hit enter to continue or Ctrl+C to cancel...')
+    input()
+    for filename in os.listdir('.'):
+        if filename.endswith('.gz'):
+            os.remove(filename)
