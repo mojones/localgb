311fd8f8df08e1fcc6311343503a4e2f58944850
martin <martin.jones@ed.ac.uk>
2017-06-30 14:24:41 +0100

    refactoring

--- localgb.py
+++ localgb.py
@@ -6,6 +6,9 @@ import logging
 import tarfile
 import pickle
 import collections
+import time
+from Bio import SeqIO
+
 
 def need_to_update_release():
     ftp = FTP('ftp.ncbi.nlm.nih.gov')
@@ -205,3 +208,157 @@ def delete_old_files():
     for filename in os.listdir('.'):
         if filename.endswith('.gz'):
             os.remove(filename)
+
+def delimited(file, delimiter='\n', bufsize=4096):
+    buf = ''
+    while True:
+        newbuf = file.read(bufsize)
+        if not newbuf:
+            yield buf
+            return
+        buf += newbuf
+        lines = buf.split(delimiter)
+        for line in lines[:-1]:
+            yield line
+        buf = lines[-1]
+
+def long_substr(data):
+    if len(data) == 1:
+        return data[0]
+    substr = ''
+    if len(data) > 1 and len(data[0]) > 0:
+        for i in range(len(data[0])):
+            for j in range(len(data[0])-i+1):
+                if j > len(substr) and all(data[0][i:i+j] in x for x in data):
+                    substr = data[0][i:i+j]
+    return substr
+
+def process_record(record, args):
+    global matching_record_count
+    global matching_feature_count
+
+
+
+    keep_record = False
+
+    # if the quick test fails, we don't want this record
+    if longest_substring not in record.lower():
+        return None
+
+    if taxid_set is not None:
+        # check if we want to allow this taxid
+        taxid_match = re.search('/db_xref="taxon:(\d+)"', record)
+        taxid = taxid_match.group(1)
+        if taxid not in taxid_set:
+            logging.debug('taxid {} not in taxids file'.format(taxid))
+            return None
+
+    # if we get this far, we want to parse the record properly
+    real_record = SeqIO.read(StringIO(record), format='gb')
+    for f in real_record.features:
+        keep_feature = False
+        if f.type == args.type:
+            if args.qualifier is None:
+                keep_feature = True
+
+            elif args.qualifier in f.qualifiers and args.terms is None:
+                keep_feature=True
+
+            elif args.qualifier in f.qualifiers and f.qualifiers[args.qualifier][0].lower() in args.terms:
+                keep_feature = True
+
+        if keep_feature:
+            matching_feature_count += 1
+            keep_record = True
+            try:
+                if args.fasta_features:
+                    output_file.write('>{}\n{}\n'.format(
+                        real_record.id,
+                        str(f.extract(real_record).seq)
+                    ))
+            except ValueError:
+                logging.debug("Couldn't extract feature from record {}".format(real_record.id))
+
+
+            if args.fasta_protein_features:
+                try:
+                    output_file.write('>{}\n{}\n'.format(
+                        real_record.id,
+                        f.qualifiers['translation'][0]
+                    ))
+                except KeyError:
+                    print('could not find a translation for feature:\n')
+                    print(f)
+
+
+
+    if keep_record:
+        matching_record_count += 1
+        if args.dump_genbank:
+            output_file.write(record + '\n//\n')
+
+longest_substring = None
+taxid_set = None
+
+matching_record_count = 0
+matching_feature_count = 0
+
+output_file = None
+
+def do_search(process_record_function, args):
+
+    global longest_substring
+    global taxid_set
+    global output_file
+
+    # figure out what the cheap check is
+    if args.terms is None and args.qualifier is None:
+        longest_substring = args.type.lower()
+    elif args.terms is None:
+        longest_substring = args.qualifier.lower()
+    else:
+        if len(args.terms) == 1:
+            longest_term_substring = args.terms[0]
+        else:
+            longest_term_substring = long_substr(args.terms).lower()
+
+        if len(longest_term_substring) > 2:
+            longest_substring = longest_term_substring
+        else:
+            longest_substring = args.qualifier.lower()
+        args.terms = [t.lower() for t in args.terms]
+
+
+    print('looking for "{}"'.format(longest_substring))
+
+    taxid_set = None
+    if args.taxid_file is not None:
+        taxid_set = set([line.rstrip('\n') for line in open(args.taxid_file)])
+
+
+
+
+    filenames = tqdm(args.files, unit='files')
+    output_file = open(args.output, 'w')
+    for filename in filenames:
+        start = time.time()
+        filenames.set_description(
+            'processing {}, found {} ({}) matching features (records)'
+            .format(os.path.basename(filename), matching_feature_count, matching_record_count)
+        )
+        if filename.endswith('.gz'):
+            genbank_file = gzip.open(filename, mode="rt", encoding='latin-1')
+        else:
+            genbank_file = open(filename, encoding='latin-1')
+
+        for record in delimited(genbank_file, '\n//\n', bufsize=4096*256):
+            process_record_function(record, args)
+        if args.verbosity > 0:
+            logging.debug("{} took {} seconds".format(
+            filename, time.time() - start
+            ))
+    output_file.close()
+
+    print('found {} matching features in {} records'.format(
+        matching_feature_count, matching_record_count)
+        )
--- query.py
+++ query.py
@@ -1,4 +1,3 @@
-from Bio import SeqIO
 import os
 from io import StringIO
 from tqdm import tqdm
@@ -7,6 +6,7 @@ import re
 import logging
 import gzip
 import time
+import localgb
 
 
 # hide gb file warnings, don't do this in production
@@ -103,146 +103,16 @@ parser.add_argument(
     '--files', nargs='+', help='genbank files to process', required=True
     )
 args = parser.parse_args()
-args.terms = [t.lower() for t in args.terms]
 
 if args.verbosity is None:
     logging.basicConfig(level=logging.INFO)
 elif args.verbosity > 0:
     logging.basicConfig(level=logging.DEBUG)
 
-#@profile
-def myreadlines(f, newline):
-  buf = ""
-  while True:
-    while newline in buf:
-      pos = buf.index(newline)
-      yield buf[:pos] + newline
-      buf = buf[pos + len(newline):]
-    chunk = f.read(4096)
-    if not chunk:
-      yield buf + newline
-      break
-    buf += chunk
-
-def delimited(file, delimiter='\n', bufsize=4096):
-    buf = ''
-    while True:
-        newbuf = file.read(bufsize)
-        if not newbuf:
-            yield buf
-            return
-        buf += newbuf
-        lines = buf.split(delimiter)
-        for line in lines[:-1]:
-            yield line
-        buf = lines[-1]
-
-def long_substr(data):
-    if len(data) == 1:
-        return data[0]
-    substr = ''
-    if len(data) > 1 and len(data[0]) > 0:
-        for i in range(len(data[0])):
-            for j in range(len(data[0])-i+1):
-                if j > len(substr) and all(data[0][i:i+j] in x for x in data):
-                    substr = data[0][i:i+j]
-    return substr
-
-def process_record(record, output_file, taxid_set=None):
-    global matching_record_count
-    global matching_feature_count
-    keep = False
-
-    # if the quick test fails, we don't want this record
-    if longest_substring not in record.lower():
-        return None
-
-    if taxid_set is not None:
-        # check if we want to allow this taxid
-        taxid_match = re.search('/db_xref="taxon:(\d+)"', record)
-        taxid = taxid_match.group(1)
-        if taxid not in taxid_set:
-            logging.debug('taxid {} not in taxids file'.format(taxid))
-            return None
-
-    # if we get this far, we want to parse the record properly
-    real_record = SeqIO.read(StringIO(record), format='gb')
-    for f in real_record.features:
-        if f.type == args.type:
-            if (
-                (
-                args.qualifier in f.qualifiers and
-                f.qualifiers[args.qualifier][0].lower() in args.terms
-                )
-                or args.qualifier is None
-             # note only looking at the first value, there can
-             # technically be a list but unlikely to be somethin
-             # we're interested in
-             ):
-                matching_feature_count += 1
-                keep = True
-                if args.fasta_features:
-                    output_file.write('>{}\n{}\n'.format(
-                        real_record.id,
-                        str(f.extract(real_record).seq)
-                    ))
-
-
-                if args.fasta_protein_features:
-                    try:
-                        output_file.write('>{}\n{}\n'.format(
-                            real_record.id,
-                            f.qualifiers['translation'][0]
-                        ))
-                    except KeyError:
-                        print('could not find a translation for feature:\n')
-                        print(f)
 
 
 
-    if keep:
-        matching_record_count += 1
-        if args.dump_genbank:
-            output_file.write(record)
-
-# figure out what the cheap check is
-if args.terms is None:
-    longest_substring = args.type.lower()
-else:
-    longest_substring = long_substr(args.terms).lower()
-
-print('looking for "{}"'.format(longest_substring))
-
-taxid_set = None
-if args.taxid_file is not None:
-    taxid_set = set([line.rstrip('\n') for line in open(args.taxid_file)])
-
-matching_record_count = 0
-matching_feature_count = 0
 
 
-def doit():
-    filenames = tqdm(args.files, unit='files')
-    with open(args.output, 'w') as output_file:
-        for filename in filenames:
-            start = time.time()
-            filenames.set_description(
-                'processing {}, found {} ({}) matching features (records)'
-                .format(os.path.basename(filename), matching_feature_count, matching_record_count)
-            )
-            if filename.endswith('.gz'):
-                genbank_file = gzip.open(filename, mode="rt", encoding='latin-1')
-            else:
-                genbank_file = open(filename, encoding='latin-1')
 
-            for record in delimited(genbank_file, '\n//\n', bufsize=4096*256):
-                process_record(record, output_file, taxid_set)
-            if args.verbosity > 0:
-                logging.debug("{} took {} seconds".format(
-                filename, time.time() - start
-                ))
-
-    print('found {} matching features in {} records'.format(
-        matching_feature_count, matching_record_count)
-        )
-doit()
+localgb.do_search(process_record_function=localgb.process_record, args=args)
